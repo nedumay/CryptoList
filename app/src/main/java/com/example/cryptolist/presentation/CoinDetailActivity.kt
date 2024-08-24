cf36f5e6f32d98c9951d91466aab593e0d94a1aa
Samim <Samim058@gamil.com>
2024-08-24 12:46:22 +0300

    Build graph

--- app/src/main/java/com/example/cryptolist/presentation/CoinDetailActivity.kt
+++ app/src/main/java/com/example/cryptolist/presentation/CoinDetailActivity.kt
@@ -2,14 +2,20 @@ package com.example.cryptolist.presentation
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.cryptolist.databinding.ActivityCoinDetailBinding
+import com.example.cryptolist.domain.CoinInfo
 import com.example.cryptolist.presentation.app.CoinApp
+import com.klim.tcharts.entities.ChartData
+import com.klim.tcharts.entities.ChartItem
 import com.squareup.picasso.Picasso
 import javax.inject.Inject
+import kotlin.random.Random
+
 
 class CoinDetailActivity : AppCompatActivity() {
 
@@ -46,8 +52,39 @@ class CoinDetailActivity : AppCompatActivity() {
                 textViewUpdate.text = it.lastUpdate
                 textViewSymbol.text = it.fromSymbol
                 Picasso.get().load(it.imageUrl).into(imageViewLogo)
+
+                //setupGraph(it, binding)
+
             }
         })
+    }
+
+    private fun setupGraph(data: CoinInfo, binding: ActivityCoinDetailBinding) {
+        val keys = ArrayList<String>() //keys for each chart
+        val names = ArrayList<String>() //names for chart
+        val colors = ArrayList<Int>() //colors for lines
+        val items = ArrayList<ChartItem>() //charts value for some time
+
+        keys.add(data.fromSymbol)
+        names.add("Green Line")
+        colors.add(Color.GREEN)
+
+        // Начало времени
+        var startTime = 1614542230000L
+        startTime+=data.lastUpdate!!.toLong()*1000
+
+        val random: Random = Random(startTime)
+        val values = ArrayList<Int>()
+        for (j in keys.indices) {
+            values.add(random.nextInt(1000))
+        }
+
+        val chartItem = ChartItem(startTime, values)
+        items.add(chartItem)
+
+        val chartData: ChartData = ChartData(keys, names, colors, items)
+        binding.graphView.setData(chartData, true)
+        binding.graphView.setTitle(String.format("Chart #%d", 1))
     }
 
     companion object{
